f71b6ebd3ee2c88267fa8981e451a4edfbfa04b7
nnthruslpn <nnthruslpn@gmail.com>
2024-06-06 08:38:56 +0000

    Deploying to gh-pages from @ nnthruslpn/twa-template@446e12d608a4fc9b18d973c9b02be18690eb6f38 🚀

--- assets/index-eba51831.js
+++ assets/index-eba51831.js
@@ -1205,6 +1205,6 @@ Values:
   padding: 20px 20px;
   overflow: hidden;
 `,xce=as.div`
-  max-width: 80%; 
-  margin: 90% auto;
+  max-width: 85%; 
+  margin: 0 auto;
 `;function Mce(){const{network:e}=Au();return Xt(wce,{children:Xt(xce,{children:_i(sg,{children:[_i(fu,{children:[Xt(_B,{}),Xt(ug,{children:e?e===$s.MAINNET?"mainnet":"testnet":"N/A"})]}),Xt(pce,{}),Xt(bce,{}),Xt(yce,{})]})})})}const _ce="https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json",Sce=new DL({defaultOptions:{queries:{refetchOnWindowFocus:!1}}});y4.createRoot(document.getElementById("root")).render(Xt(UZ,{manifestUrl:_ce,children:Xt(iD,{client:Sce,children:Xt(Mce,{})})}));
